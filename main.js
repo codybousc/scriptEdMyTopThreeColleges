192fc2ef41e2ab3d1228a57c147a88f454ebf294
Cody Bouscaren <codybousc@gmail.com>
2016-04-19 12:44:19 -0400

    Practice files

---
+++ main.js
@@ -0,0 +1,47 @@
+var myTopThreeColleges = {};
+
+/*
+1) Add three college objects to the myTopThreeColleges object (refer to daily docs if needed).
+  Each college must contain:
+    a) avgGPA (an integer/float)
+    b) avgSAT (an integer)
+    c) 3 potential majors you're interested in studying (an array)
+*/
+
+
+
+
+/*
+2) console.log these statements for each college
+  My first choice is COLLEGE NAME. In order to get in, I’ll need an average gpa of AVERAGE GPA.
+  Three majors that I’d be interested in studying at COLLEGE NAME are 3 POTENTIAL MAJORS
+  (HINT: You'll need to iterate over the array to get to the college names).
+*/
+
+
+
+
+/*
+3) Change at least one of the majors in one of your favorite colleges (using either dot or bracket notation)
+and console.log the following statement.
+  After giving it some thought, I think I might be interested in studying 3 POTENTIAL MAJORS at COLLEGE NAME.
+*/
+
+
+
+
+/*
+4) Wrap the console.logs in a function and call it/log it once for each college in the myTopThreeColleges object.
+*/
+
+
+
+
+/*
+5) Create a nested object of your choice. It must contain an integer, an array and another object as values.
+Iterate over the object and attempt to print out all of the values. Are you able to access the individual values
+within the array and the object? See if you can use recursion to log out all elements in the arrray and object as
+you iterate over the object. Here’s a hint:
+
+http://stackoverflow.com/questions/5432967/how-to-iterate-over-inner-objects-property-in-an-object
+*/
